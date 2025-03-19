76ceab887d5db55a944f505f652f533fe0f8e9fa
R0manOza <161681342+R0manOza@users.noreply.github.com>
2025-03-19 10:42:03 +0400

    added more testcases into update

--- test/algorithmTest.ts
+++ test/algorithmTest.ts
@@ -173,14 +173,30 @@ describe("practice()", () => {
  * 1. demote card to bucket 0 when difficulty is 0
  */
 describe("update()", () => {
-  it("should demote card to bucket 0 from bucket 1  when difficulty is 0", () => {
+  it("should demote card to bucket 0   when difficulty is 0", () => {
     let card = new Flashcard("Question", "Answer", "Hint", ["tag"]);
     let bucketmap = new Map<number, Set<Flashcard>>();
     bucketmap.set(1, new Set([card]));
     let updated = update(bucketmap, card, 0);
     assert(updated.get(0)?.has(card));
   });
-  
+  it("should promote card to next bucket when difficulty is 1", () => {
+    let card = new Flashcard("Question", "Answer", "Hint", ["tag"]);
+    let bucketmap = new Map<number, Set<Flashcard>>();
+    bucketmap.set(0, new Set([card]));
+    bucketmap.set(1, new Set([]));
+    let updated = update(bucketmap, card, 1);
+    assert(updated.get(1)?.has(card));
+  });
+  it("should demote by one bucket when difficulty is 2", () => {
+    let card = new Flashcard("Question", "Answer", "Hint", ["tag"]);
+    let bucketmap = new Map<number, Set<Flashcard>>();
+    bucketmap.set(0, new Set([]));
+    bucketmap.set(1, new Set([]));
+    bucketmap.set(2, new Set([card]));
+    let updated = update(bucketmap, card, 2);
+    assert(updated.get(1)?.has(card));
+  });
 });
 
 /*
